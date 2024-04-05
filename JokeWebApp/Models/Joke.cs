4b3e9a0589ea591a08d14688ed67e0370566acab
Gavin <Gavin@GAVIN-PC>
2024-04-05 10:20:05 -0400

    Add project files.

---
+++ JokeWebApp/Models/Joke.cs
@@ -0,0 +1,17 @@
+﻿namespace JokeWebApp.Models
+{
+    public class Joke
+    {
+        public string Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+
+
+
+        public Joke()
+        {
+            
+        }
+    }
+}
